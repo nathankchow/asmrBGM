a4286b6b305d32eccc4a48d56d85e019eeb38c8f
Nathan Chow <nathankchow@ucsb.edu>
2022-04-13 19:05:52 -0700

    Initial Commit

---
+++ asmrBGM/asmrBGMApp.swift
@@ -0,0 +1,17 @@
+//
+//  asmrBGMApp.swift
+//  asmrBGM
+//
+//  Created by natha on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct asmrBGMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

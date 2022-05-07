e393cfa104b18282c570fd13c8067164a7b3c082
Nathan Chow <nathankchow@ucsb.edu>
2022-05-07 14:20:13 -0700

    re-arranged pre-existing buttons
    Former-commit-id: 08f79a8e245847ab137ec6ae528f750e02cc6404 [formerly 4429aa81bbee7cb1f52ebfdf5a473a5ae10b5b7d]
    Former-commit-id: 2236c748585be3526ab26a6f48d70f102983bf6a

--- asmrBGM/asperi.swift
+++ asmrBGM/asperi.swift
@@ -27,36 +27,32 @@ struct asperi: View {
     var body: some View {
         NavigationView{
             VStack {
-                Text("\(self.$audiosettings.asmrtrack.title.wrappedValue)\n\(self.$audiosettings.bgmtrack.title.wrappedValue)")
-
-                Button(action: {
-                    if (self.playButton == Image(systemName: "play.circle")) {
-                        print("All Done")
-                        self.audiosettings.playSound(sound: audiosettings.testSound, type: "mp3")
-                        self.audiosettings.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-                        self.playButton = Image(systemName: "pause.circle")
-                        
-                    } else {
-                        print("Not done")
-                        self.audiosettings.pauseSound()
-                        self.playButton = Image(systemName: "play.circle")
+                
+                Group {
+                    NavigationLink(destination: loadAsmr(asmrtrack: self.$audiosettings.asmrtrack), tag: 1, selection: $page) {
+                        EmptyView()
                     }
-                }) {
-                    self.playButton
-                        .foregroundColor(Color.blue)
-                        .font(.system(size: 44))
+                    NavigationLink(destination: loadBgm(bgmtrack: self.$audiosettings.bgmtrack), tag: 2, selection: $page) {
+                        EmptyView()
+                    }
                 }
-                Button(action: {
-                    print("All Done")
-                    self.audiosettings.stopSound()
-                    self.playButton = Image(systemName: "play.circle")
-                    self.audiosettings.playValue = 0.0
-                    
-                }) {
-                    Image(systemName: "stop.circle")
-                        .foregroundColor(Color.green)
-                        .font(.system(size: 44))
+                
+                Group {
+                    Text("\(self.$audiosettings.asmrtrack.title.wrappedValue)\n\(self.$audiosettings.asmrtrack.artist.wrappedValue)")
+                    Button(action: {
+                        self.page = 1
+                    }) {
+                        Text("Load ASMR file")
+                    }
+                    Text("\(self.$audiosettings.bgmtrack.title.wrappedValue)\n\(self.$audiosettings.bgmtrack.artist.wrappedValue)")
+                    Button(action: {
+                        self.page = 2
+                    }) {
+                        Text("Load BGM file")
+                    }
                 }
+                
+                Group {
             
                 Slider(value: $audiosettings.playValue, in: TimeInterval(0.0)...audiosettings.playerDuration!, onEditingChanged: { editing in
                 self.audiosettings.isPositionEditing = editing
@@ -82,38 +78,41 @@ struct asperi: View {
                         self.audiosettings.timer.upstream.connect().cancel()
                     }
                 }
-                Button(action: {
-                    self.audiosettings.changeSong()
-                    self.pauseToPlay()
-                    self.audiosettings.playSound(sound: audiosettings.testSound, type: "mp3")
-                    self.audiosettings.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-                    self.playButton = Image(systemName: "pause.circle")
-                    
-                }) {
-                    Text("CHANGE SONG TESTING")
-                }
+                    Button(action: {
+                        if (self.playButton == Image(systemName: "play.circle")) {
+                            print("All Done")
+                            self.audiosettings.playSound(sound: audiosettings.testSound, type: "mp3")
+                            self.audiosettings.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                            self.playButton = Image(systemName: "pause.circle")
+                            
+                        } else {
+                            print("Not done")
+                            self.audiosettings.pauseSound()
+                            self.playButton = Image(systemName: "play.circle")
+                        }
+                    }) {
+                        self.playButton
+                            .foregroundColor(Color.blue)
+                            .font(.system(size: 44))
+                    }
+//                Button(action: {
+//                    self.audiosettings.changeSong()
+//                    self.pauseToPlay()
+//                    self.audiosettings.playSound(sound: audiosettings.testSound, type: "mp3")
+//                    self.audiosettings.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+//                    self.playButton = Image(systemName: "pause.circle")
+//
+//                }) {
+//                    Text("CHANGE SONG TESTING")
+//                }
 
                 
-                NavigationLink(destination: loadAsmr(asmrtrack: self.$audiosettings.asmrtrack), tag: 1, selection: $page) {
-                    EmptyView()
-                }
 
-                NavigationLink(destination: loadBgm(bgmtrack: self.$audiosettings.bgmtrack), tag: 2, selection: $page) {
-                    EmptyView()
-                }
                 
-                Button(action: {
-                    self.page = 1
-                }) {
-                    Text("Load ASMR file")
-                }
 
-                Button(action: {
-                    self.page = 2
-                }) {
-                    Text("Load BGM file")
+
+
                 }
-                
 
             }
         }
